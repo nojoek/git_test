005bf8ef818b0cb27f6fa1338b050cc67e894d74
Joseph Patt <J@Josephs-MacBook-Air.local>
2013-05-10 15:59:09 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitTest::Application.config.secret_token = 'c9ae78b85b27b109b63d0e7cf58950e11173fa927cc2a8c5d65aee55cc4828f84bedf1e10ef6cb53c17c4fcb41292d9d286abe72d6cee9331ccdf6d50941eb14'
